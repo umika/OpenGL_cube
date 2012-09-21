b6f4b5acf26cd4f6f6823a1b3d0ae6758e3bb92f
umika <9umika@gmail.com>
2012-09-21 17:54:17 +0900

    change files resource/72dpi*.png LoadTextures uses custom loader pngLoadRaw and 32bit png with alpha

--- rcube.cpp
+++ rcube.cpp
@@ -10,6 +10,7 @@
   'GL/glpng.h' 'glpng.lib' from http://openports.se/graphics/glpng
 */
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <cstdarg>
@@ -81,23 +82,71 @@ int main(int argc, char **argv)
 
 unsigned char AlphaCallback(unsigned char r, unsigned char g, unsigned char b)
 {
-  unsigned a = 255 - (r + g + b) / 3;
+  // unsigned a = 255 - (r + g + b) / 3;
+  unsigned a = 255 * (r + g + b) / (255 * 3);
   return a & 0x00FF;
 }
 
 void LoadTextures(void)
 {
-  pngSetAlphaCallback(AlphaCallback);
   string *a[] = {&TEXIMG_HINT, &TEXIMG_CHAR[0]};
   for(int i = 0; i < sizeof(a) / sizeof(string *); i++){
+    string &filename = path_join(2, &RESOURCE_DIR, a[i]);
+#if 1 // Using pngLoadRaw and setting texture parameters manually.
+    glGenTextures(1, &Gtextures[i]);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, Gtextures[i]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    pngRawInfo pri;
+    if(!pngLoadRaw(filename.c_str(), &pri)){
+      cerr << "Can't load file: " << *a[i];
+      exit(1);
+    }else{
+#if _DEBUG
+      cout << "File: " << *a[i] << endl;
+      cout << " Width: " << pri.Width << endl;
+      cout << " Height: " << pri.Height << endl;
+      cout << " Depth: " << pri.Depth << endl;
+      cout << " Alpha: " << pri.Alpha << endl;
+      cout << " Components: " << pri.Components << endl;
+      cout << " pData: " << (long)pri.Data << endl;
+      cout << " pPalette: " << (long)pri.Palette << endl;
+#endif
+      if(pri.Data){
+        unsigned char buf[4 * 256 * 256];
+        for(int y = 0; y < pri.Height; y++){
+          for(int x = 0; x < pri.Width; x++){
+            int r = y * pri.Width + x;
+            int q = r * 4;
+            if(pri.Components == 1){
+              int p = pri.Data[r] * 3;
+              for(int j = 0; j < 3; j++) buf[q + j] = pri.Palette[p + j];
+            }else{
+              for(int j = 0; j < 3; j++) buf[q + j] = pri.Data[q + j];
+            }
+            buf[q + 3] = AlphaCallback(buf[q + 0], buf[q + 1], buf[q + 2]);
+          }
+        }
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, pri.Width, pri.Height, 0,
+          GL_RGBA, GL_UNSIGNED_BYTE, buf);
+        free(pri.Data);
+      }
+      if(pri.Palette) free(pri.Palette);
+    }
+#else
     pngInfo pi;
-    Gtextures[i] = pngBind(path_join(2, &RESOURCE_DIR, a[i]).c_str(),
-      PNG_NOMIPMAP, PNG_CALLBACK /* PNG_ALPHA */,
-      &pi, GL_CLAMP, GL_NEAREST, GL_NEAREST);
+    pngSetAlphaCallback(AlphaCallback);
+    Gtextures[i] = pngBind(filename.c_str(), PNG_NOMIPMAP,
+      PNG_CALLBACK /* PNG_ALPHA */, &pi, GL_CLAMP, GL_NEAREST, GL_NEAREST);
     if(!Gtextures[i]){
       cerr << "Can't load file: " << *a[i];
       exit(1);
     }
+#endif
   }
 }
 
@@ -126,9 +175,9 @@ void Display(void)
   glLoadIdentity();
   glColor4f(1.0, 1.0, 1.0, 1.0);
   glEnable(GL_TEXTURE_2D);
+//  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 //  glBlendFunc(GL_SRC_ALPHA, GL_SRC_COLOR);
   glBlendFunc(GL_SRC_COLOR, GL_SRC_ALPHA);
-//  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_BLEND);
 //  glPushMatrix();
     glTranslatef(0.0, 0.0, -5.0);
